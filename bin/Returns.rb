d79315439eb3997c6e2ee5478b4940c8e57729f8
Nicholas Weigand <dawtsf@gmail.com>
2013-04-08 16:27:21 -0700

    Fixed a bunch of typos etc.  Also got rid of mutexes in favor of a local variable

--- bin/Returns.rb
+++ bin/Returns.rb
@@ -25,25 +25,25 @@ module Returns
 			@userField = ReturnsConfig::Structure::USERFIELD
 			@passField = ReturnsConfig::Structure::PASSFIELD
 			@loginButton = ReturnsConfig::Structure::LOGINBUTTON
-			@notAuthText = ReturnsConfig::Structure::NOAUTHID
+			@notAuthText = ReturnsConfig::Structure::NOAUTHTEXT
 			@editLink = ReturnsConfig::Structure::EDITLINK
 			@createLink = ReturnsConfig::Structure::CREATELINK
 			@nrText = ReturnsConfig::Structure::NRTEXT
 			@penalizedText = ReturnsConfig::Structure::PENALIZEDTEXT
-			@itemCountName = ReturnsConfig::Structure::ITEMCOUNTTEXT
+			@itemCountName = ReturnsConfig::Structure::ITEMCOUNTNAME
 			@updateName = ReturnsConfig::Structure::UPDATENAME
 			@productField = ReturnsConfig::Structure::PRODUCTFIELD
 			@addButton = ReturnsConfig::Structure::ADDBUTTON
 			@errorClass = ReturnsConfig::Structure::ERRORCLASS
 
-			@semaphore = Mutex.new
+			@stoppable = true
 			@b = Watir::Browser.new @browserType
 		end
 
 		def login
 			@b.goto @loginPage
-			@b.text_field(:id => userField).set @user
-			@b.text_field(:id => passField).set @password
+			@b.text_field(:id => @userField).set @user
+			@b.text_field(:id => @passField).set @password
 			@b.button(:id => @loginButton).click
 
 			if @b.text.include? @notAuthText
@@ -65,73 +65,32 @@ module Returns
 		end
 
 		def start
+			@stoppable = true
 			self.login
 			self.open_returns
 			loop do
-				#this isbn isn't mutex protected, but the data will be correct
 				@isbn = inQ.pop
-				@semaphore.synchronize do
-					@b.text_field(:name => @productField).set @isbn
-					@b.button(:value => @addButton).click
-
-					if @b.div(:class => @errorClass, :text => @nrText).exists?
-						nonReturnableQ << @isbn
-					elsif @b.div(:class => @errorClass, :text => @penalizedText).exists?
-						penalizedQ << @isbn
-						unless @ignorePenalties
-							(@b.text_field(:name => @itemCountName).set(b.text_field(:name => @itemCountName).value.to_i - 1))
-							@b.button(:name => @updateName).click
-						end
+				@stoppable = false
+				@b.text_field(:name => @productField).set @isbn
+				@b.button(:value => @addButton).click
+				if @b.div(:class => @errorClass, :text => @nrText).exists?
+					nonReturnableQ << @isbn
+				elsif @b.div(:class => @errorClass, :text => @penalizedText).exists?
+					penalizedQ << @isbn
+					unless @ignorePenalties
+						(@b.text_field(:name => @itemCountName).set(b.text_field(:name => @itemCountName).value.to_i - 1))
+						@b.button(:name => @updateName).click
 					end
-					processedQ << @isbn
 				end
+				processedQ << @isbn
+				@stoppable = true
 			end
 		end
 
 		def stop
-			while @semaphore.locked?
+			until @stoppable
 			end
 			@b.close
 		end
 	end
-end
-
-inQ = Queue.new
-processedQ = Queue.new
-nonReturnableQ = Queue.new
-penalizedQ = Queue.new
-threads = []
-
-ReturnsConfig::THREADS.times do
-	threads << Thread.new do
-		begin
-			r = Returner.new
-			r.start
-		ensure
-			r.stop
-		end
-	end
-end
-
-STDIN.each_line do |isbn|
-	inQ << isbn
-end
-
-threads.each do |thread|
-	thread.exit
-end
-
-puts "Processed:"
-until processedQ.empty? do
-	puts "#{processedQ.pop}"
-end
-
-puts "Penalized:"
-until penalizedQ.empty? do
-	puts "#{penalizedQ.pop}"
-end
-
-puts "NonReturnable:"
-until nonReturnableQ.empty? do
-	puts "#{nonReturnableQ.pop}"
 end
